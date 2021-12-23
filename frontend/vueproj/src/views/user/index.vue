f8f891fde03f0e20dfa5f0edba2d40def7669187
gzn_jjw <jjw_0418@naver.com>
2021-12-23 18:00:00 +0900

    frontend: vue.js
     - 유저 관리 페이지 생성
     - index.vue
     - inform.vue

--- frontend/vueproj/src/views/user/index.vue
+++ frontend/vueproj/src/views/user/index.vue
@@ -1,9 +1,128 @@
 <template>
-  <div><h1>사용자 관리</h1></div>
+  <div>
+    <h1>사용자 관리</h1>
+    <div style="margn-bottom: 5px">
+      <b-row>
+        <b-col>
+          <b-input-group>
+            <b-form-input
+              v-model="searchParams"
+              placeholder="검색"
+              @keyup.enter="searchUserList"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button variant="outline-success" @click="searchUserList()"
+                >검색</b-button
+              >
+            </b-input-group-append>
+          </b-input-group>
+        </b-col>
+        <b-col style="text-align: right">
+          <b-button variant="success" size="sm" @click="onClickAddNew"
+            >신규등록</b-button
+          >
+        </b-col>
+      </b-row>
+    </div>
+    <div>
+      <!-- data -> items(userList) => item(Department) -> (name) ->  -->
+      <b-table striped hover :items="userList" :fields="fields">
+        <template #cell(Department)="data">
+          {{ data.item.Department.name }}
+        </template>
+        <template #cell(updateBtn)="data">
+          <b-button variant="success" @click="onClickEdit(data.item.id)"
+            >수정</b-button
+          >
+          {{ data.item.update }}
+        </template>
+        <template #cell(deleteBtn)="data">
+          <b-button variant="danger" @click="onClickDelete(data.item.id)"
+            >삭제</b-button
+          >
+          {{ data.item.delete }}
+        </template>
+      </b-table>
+    </div>
+    <!-- 입력 폼 -->
+    <inform />
+  </div>
 </template>
 
 <script>
-export default {};
+import inform from "./inform.vue";
+export default {
+  components: {
+    inform: inform,
+  },
+  data() {
+    return {
+      searchParams: null,
+      fields: [
+        { key: "id", label: "id" },
+        { key: "name", label: "이름" },
+        { key: "Department", label: "부서Id" },
+        { key: "userid", label: "아이디" },
+        { key: "role", label: "권한" },
+        { key: "email", label: "이메일" },
+        { key: "createdAt", label: "생성일" },
+        { key: "updateBtn", label: "수정" },
+        { key: "deleteBtn", label: "삭제" },
+      ],
+
+      userList: [
+        {
+          id: 1,
+          departmentId: 1,
+          name: "홍길동",
+          userid: "hong",
+          role: "leader",
+          email: "hong@email.com",
+          phone: "010-1234-5678",
+          createdAt: "2021-12-01T00:00:00.000Z".substring(0, 10),
+          Department: {
+            id: 1,
+            name: "개발팀",
+            code: "dev",
+            createdAt: "2021-12-01T00:00:00.000Z",
+          },
+        },
+
+        {
+          id: 2,
+          departmentId: 2,
+          name: "김길동",
+          userid: "kim",
+          role: "member",
+          email: "kim@email.com",
+          phone: "010-9876-5432",
+          createdAt: "2021-12-01T00:00:00.000Z".substring(0, 10),
+          Department: {
+            id: 2,
+            name: "영업팀",
+            code: "sales",
+            createdAt: "2021-12-01T00:00:00.000Z",
+          },
+        },
+      ],
+    };
+  },
+  methods: {
+    searchUserList() {
+      console.log("searchUserList");
+    },
+    onClickAddNew() {
+      console.log("onClickAddNew", this.searchParams);
+      this.$bvModal.show("modal-user-inform");
+    },
+    onClickEdit(id) {
+      console.log(id);
+    },
+    onClickDelete(id) {
+      console.log(id);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
---
+++ frontend/vueproj/src/views/user/inform.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <b-modal id="modal-user-inform" title="BootstrapVue" @ok="onSubmit">
+      <b-form-group
+        label-cols="4"
+        label-cols-lg="2"
+        label="부서PK"
+        label-for="departmentId"
+      >
+        <b-form-input
+          id="departmentId"
+          v-model="user.departmentId"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        label-cols="4"
+        label-cols-lg="2"
+        label="이름"
+        label-for="name"
+      >
+        <b-form-input id="name" v-model="user.name"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        label-cols="4"
+        label-cols-lg="2"
+        label="아이디"
+        label-for="id"
+      >
+        <b-form-input id="id" v-model="user.id"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        label-cols="4"
+        label-cols-lg="2"
+        label="비밀번호"
+        label-for="password"
+      >
+        <b-form-input id="password" v-model="user.password"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        label-cols="4"
+        label-cols-lg="2"
+        label="사용자 권한"
+        label-for="role"
+      >
+        <b-form-radio-group
+          v-model="user.role"
+          :options="userRole.options"
+        ></b-form-radio-group>
+      </b-form-group>
+      <b-form-group
+        label-cols="4"
+        label-cols-lg="2"
+        label="이메일"
+        label-for="email"
+      >
+        <b-form-input id="email" v-model="user.email"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        label-cols="4"
+        label-cols-lg="2"
+        label="전화번호"
+        label-for="phone"
+      >
+        <b-form-input id="phone" v-model="user.phone"></b-form-input>
+      </b-form-group>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        id: null,
+        departmentId: null,
+        name: null,
+        userid: null,
+        password: null,
+        role: null,
+        email: null,
+        phone: null,
+        updatedPwDate: null,
+        createdAt: null,
+      },
+      userRole: {
+        default: "member",
+        options: [
+          { value: "leader", text: "팀장" },
+          { value: "member", text: "팀원" },
+        ],
+      },
+    };
+  },
+
+  created() {
+    // 초기값 세팅 함수 호출
+    this.setDefaultValues();
+  },
+  methods: {
+    onSubmit() {
+      console.log("onsubmit", this.user);
+    },
+    setDefaultValues() {
+      // 초기값 세팅
+      this.user.role = this.userRole.default;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
