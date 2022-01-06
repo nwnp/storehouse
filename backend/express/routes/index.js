f67490b3eac5cd2477ebb3d4566fb4d5632b759e
gzn_jjw <jjw_0418@naver.com>
2022-01-06 17:43:57 +0900

    feat: use User model
     - GET / : findAll()
     - POST / : create()

--- backend/express/routes/index.js
+++ backend/express/routes/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const { User } = require("../models");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log(req.url, "routing");
-  res.send("<h1>Hello World!!!</h1>");
+router.get("/", async (req, res, next) => {
+  // console.log(req.url, "routing");
+  // res.send("<h1>Hello World!!!</h1>");
+  try {
+    const users = await User.findAll();
+    // res.render("sequelize", { users });
+    res.send(users);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
--- backend/express/routes/user.js
+++ backend/express/routes/user.js
@@ -1,13 +1,44 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  // console.log(req.url);
-  const data = JSON.stringify(req.body);
-  console.log(`post body: ${data}`);
-  res.send("post 실행");
-});
+const { User } = require("../models");
+
+router
+  .get("/", (req, res) => {
+    res.send("<h1>GET 방식 / </h1>");
+  })
+  .get("/:id", (req, res) => {
+    // const { id } = req.param;
+    console.log(`req.params.id: ${req.params.id}`);
+    res.status(200).send(users);
+  })
+  .post("/", async (req, res, next) => {
+    // console.log(req.url);
+    // const userData = req.body;
+    // console.log(`post body: ${JSON.stringify(userData)}`);
+    try {
+      const user = await User.create({
+        name: req.body.name,
+        description: req.body.description,
+      });
+
+      console.log(user);
+      // res.status(201).json(user);
+      res.send(user);
+    } catch (err) {
+      console.log("=============================");
+      console.error(err);
+      console.log("=============================");
+      next(err);
+    }
+
+    // users.push(userData); // 임시 유저 추가
+    // console.log(users);
+    // res.send("post 실행");
+  });
+// .get("/like", (req, res) => {
+//   res.send("like page");
+// }); // like가 문자열이기 때문에 :id의 파라미터로 인식을 하고 /:id 라우팅이 됨
 
 module.exports = router;
