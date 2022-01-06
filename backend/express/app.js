2e41a43bd79f7600913c18898680a37ebf3fb300
gzn_jjw <jjw_0418@naver.com>
2022-01-06 17:39:57 +0900

    feat: connect with database
     - app.js에서 postgres 연결

--- backend/express/app.js
+++ backend/express/app.js
@@ -6,11 +6,26 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const path = require("path");
 
+const { sequelize } = require("./models");
+
 const app = express();
 const PORT = 8080;
 
-dotenv.config();
+// connecting database: database_development
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("postgres 데이터베이스(database_development) 연결 성공");
+  })
+  .catch((err) => {
+    console.error(`데이터베이스 연결 실패 error: ${err}`);
+  });
 
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/user");
+
+// middlewares
+dotenv.config();
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,28 +44,24 @@ app.use(
   })
 );
 
-const indexRouter = require("./routes/index");
-const userRouter = require("./routes/user");
-
+// all requests
 app.use((req, res, next) => {
   console.log("모든 요청에서 다 실행됨");
   console.log(req.url, req.params, req.path);
   next();
 });
 
+// routing
 app.use("/", indexRouter);
-app.use("/", userRouter);
-// app.post("/", (req, res) => {
-//   console.log(req.body);
-//   res.send("post 실행");
-// });
+app.use("/users", usersRouter);
 
-// 경로를 못 찾을때 실행됨
+// no path
 app.use((req, res, next) => {
   console.log("경로를 못 찾았을 때 실행됨");
   res.status(404).send("Not Found");
 });
 
+// listen
 app.listen(PORT, () => {
   console.log(`${PORT}번에서 대기 중....`);
 });
